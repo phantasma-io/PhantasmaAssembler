9fcb368844f8633157109419efbc36cd397b145d
Bruno Freitas <bruno_freitas_2015@outlook.com>
2018-07-18 10:26:46 +0100

    Initial commit

---
+++ PhantasmaAssembler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhantasmaAssembler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
